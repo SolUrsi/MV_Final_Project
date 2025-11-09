3ad7e18011c8f869716cf0ea9a633c8ed6402d55
Fynn <80987925+f3Y0@users.noreply.github.com>
2025-11-09 18:49:34 +0100

    gestures fix

--- gesturebot_ws/src/gesturebot_gestures/gesturebot_gestures/gesture_detector.py
+++ gesturebot_ws/src/gesturebot_gestures/gesturebot_gestures/gesture_detector.py
@@ -1,13 +1,14 @@
+from collections import Counter, deque
+from enum import Enum
+
 import cv2
 import mediapipe as mp
 import numpy as np
 import rclpy
-
-from collections import Counter, deque
-from enum import Enum
 from geometry_msgs.msg import Twist
 from gesturebot_msgs.msg import ArmCommand, GripperCommand
 from rclpy.node import Node
+
 
 class Direction(Enum):
     FORWARD = 0
@@ -17,15 +18,18 @@ class Direction(Enum):
     NONE = 4
     BACKWARD = 5
 
+
 class GestureDetector(Node):
 
     def __init__(self):
-        super().__init__('gesture_detector')
+        super().__init__("gesture_detector")
 
-        self.vel_publisher = self.create_publisher(Twist, 'cmd_vel', 10)
-        self.gripper_publisher = self.create_publisher(GripperCommand, 'gripper_motion', 10)
-        self.arm_publisher = self.create_publisher(ArmCommand, 'arm_motion', 10)
-        
+        self.vel_publisher = self.create_publisher(Twist, "cmd_vel", 10)
+        self.gripper_publisher = self.create_publisher(
+            GripperCommand, "gripper_motion", 10
+        )
+        self.arm_publisher = self.create_publisher(ArmCommand, "arm_motion", 10)
+
         self.mp_hands = mp.solutions.hands
         self.mp_drawing = mp.solutions.drawing_utils
 
@@ -33,7 +37,7 @@ class GestureDetector(Node):
             static_image_mode=False,
             max_num_hands=2,
             min_detection_confidence=0.7,
-            min_tracking_confidence=0.5
+            min_tracking_confidence=0.5,
         )
 
         self.detect_gestures()
@@ -45,11 +49,13 @@ class GestureDetector(Node):
         return np.degrees(np.arccos(np.clip(np.dot(v1, v2), -1.0, 1.0)))
 
     def finger_curl(self, landmarks, ids):
-        mcp, pip, tip = [np.array([landmarks[i].x, landmarks[i].y, landmarks[i].z]) for i in ids]
+        mcp, pip, tip = [
+            np.array([landmarks[i].x, landmarks[i].y, landmarks[i].z]) for i in ids
+        ]
         v1 = pip - mcp
         v2 = tip - pip
         angle = self.angle_between(v1, v2)
-        
+
         straight_deg = 180
         bent_deg = 12
         curl = np.clip((straight_deg - angle) / (straight_deg - bent_deg), 0, 1)
@@ -57,14 +63,14 @@ class GestureDetector(Node):
 
     def hand_closure_ratio(self, landmarks):
         fingers = {
-            'index': [5, 6, 8],
-            'middle': [9, 10, 12],
-            'ring': [13, 14, 16],
-            'pinky': [17, 18, 20],
+            "index": [5, 6, 8],
+            "middle": [9, 10, 12],
+            "ring": [13, 14, 16],
+            "pinky": [17, 18, 20],
         }
         curls = [self.finger_curl(landmarks, ids) for ids in fingers.values()]
         return np.mean(curls)
-    
+
     def detect_commands(self, hand_landmarks):
         lm = hand_landmarks.landmark
         fingers = []
@@ -74,9 +80,9 @@ class GestureDetector(Node):
             fingers.append(1)
         else:
             fingers.append(0)
-        
+
         for finger in [8, 12, 16, 20]:
-            if lm[finger].y < lm[finger -1].y:
+            if lm[finger].y < lm[finger - 1].y:
                 fingers.append(1)
             else:
                 fingers.append(0)
@@ -84,19 +90,19 @@ class GestureDetector(Node):
         # [thumb, index, middle, ring, pinky]
         if fingers == [0, 1, 0, 0, 0]:
             direction = Direction.FORWARD
-        elif fingers == [0, 0, 0, 0, 0]:
-            direction = Direction.STOP
         elif fingers == [0, 1, 1, 1, 1]:
             direction = Direction.LEFT
         elif fingers == [1, 0, 0, 0, 0]:
             direction = Direction.RIGHT
         elif fingers == [0, 1, 1, 0, 0]:
-            direction == Direction.BACKWARD
+            direction = Direction.BACKWARD
+        elif fingers == [0, 0, 0, 0, 0]:
+            direction = Direction.STOP
 
         return direction
 
     def detect_gestures(self):
-        PIXEL_TO_M = (1/8)*(1/100)
+        PIXEL_TO_M = (1 / 8) * (1 / 100)
 
         prev_x = None
         prev_y = None
@@ -121,7 +127,7 @@ class GestureDetector(Node):
 
             frame = cv2.flip(frame, 1)
             h, w, _ = frame.shape
-            center_x, center_y = int((3/4)*w), h // 2
+            center_x, center_y = int((3 / 4) * w), h // 2
 
             rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             result = self.hands.process(rgb)
@@ -135,10 +141,15 @@ class GestureDetector(Node):
                     direction = Direction.STOP
 
                     if handedness == "Right":
-                        gripper_closure = self.hand_closure_ratio(hand_landmarks.landmark)
+                        gripper_closure = self.hand_closure_ratio(
+                            hand_landmarks.landmark
+                        )
                         gripper_closure = int(gripper_closure * 100)
                         if prev_closure:
-                            gripper_closure = alpha_gripper*gripper_closure + (1-alpha_gripper)*prev_closure
+                            gripper_closure = (
+                                alpha_gripper * gripper_closure
+                                + (1 - alpha_gripper) * prev_closure
+                            )
                         gripper_closure = int(gripper_closure)
                         prev_closure = gripper_closure
 
@@ -146,7 +157,9 @@ class GestureDetector(Node):
                         gripper_msg.gripper_percentage = gripper_closure
                         self.gripper_publisher.publish(gripper_msg)
 
-                        lm = hand_landmarks.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_MCP]
+                        lm = hand_landmarks.landmark[
+                            self.mp_hands.HandLandmark.MIDDLE_FINGER_MCP
+                        ]
                         pixel_x = int(lm.x * w)
                         pixel_y = int(lm.y * h)
 
@@ -157,11 +170,11 @@ class GestureDetector(Node):
                         robot_y = -rel_x
 
                         if prev_x:
-                            robot_x = alpha*robot_x + (1-alpha)*prev_x
+                            robot_x = alpha * robot_x + (1 - alpha) * prev_x
                         prev_x = robot_x
-                        
+
                         if prev_y:
-                            robot_y = alpha*robot_y + (1-alpha)*prev_y
+                            robot_y = alpha * robot_y + (1 - alpha) * prev_y
                         prev_y = robot_y
 
                         arm_msg = ArmCommand()
@@ -193,8 +206,12 @@ class GestureDetector(Node):
 
                     self.vel_publisher.publish(vel_msg)
 
-                    self.get_logger().info(f"x={robot_x:.2f}m, y={robot_y:.2f}m, closure={gripper_closure}, direction={direction}")
-                    self.mp_drawing.draw_landmarks(frame, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
+                    self.get_logger().info(
+                        f"x={robot_x:.2f}m, y={robot_y:.2f}m, closure={gripper_closure}, direction={direction}"
+                    )
+                    self.mp_drawing.draw_landmarks(
+                        frame, hand_landmarks, self.mp_hands.HAND_CONNECTIONS
+                    )
 
             cv2.imshow("MediaPipe Hands", frame)
 
@@ -203,6 +220,7 @@ class GestureDetector(Node):
 
         cap.release()
         cv2.destroyAllWindows()
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -214,5 +232,6 @@ def main(args=None):
     gesture_detector.destroy_node()
     rclpy.shutdown()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
